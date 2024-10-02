a9211b5b45f125fde4248e5c6de8355775892c90
Zelalem Yalew <zyalew@cs.loyola.edu>
2024-10-02 13:16:02 -0400

    Lab0 Final Grade and Feedback

---
+++ lab0_best_model.py
@@ -0,0 +1,29 @@
+# Programmers: Harry Li & Andrew Leimbach
+# Course:  CS151, Dr. Zee
+# Due Date: 9/12/24
+# Lab Assignment: 0
+# Problem Statement:  Output information about the programmers
+# Data In: None, there is no user input in this program
+# Data Out:  The names of the programmers and five statements about each of them
+# Credits: Code based on class assignment
+
+
+print ("CS151 Lab 0") #The code outputs "CS151 Lab 0"
+
+print("My partner is Andrew Leimbach.") #code prints the name of my partner
+
+#codes outputs 5 different facts about my partner
+print("He is from Ellicott City, Maryland.")
+print("He is a Data Science Major.")
+print("He lives in Hammerman Hall.")
+print("His dad and brother are both Aluminis.")
+print("His favorite sports are golf and basketball.")
+
+print("My partner is Harry Li.") #code prints the name of my partner
+
+#codes outputs 5 different facts about my partner
+print("He is from Baltimore County, Maryland.")
+print("He is a Computer Science Major.")
+print("He lives in Thea Bowman Hall.")
+print("He plays badminton and tennis.")
+print("He has traveled to China three times.")
